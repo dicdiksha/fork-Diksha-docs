9ae27be1de2179e237d2feb365db3ca9e1f86fd0
Baijanti Yadav <baijanti.yadav@trigyn.com>
2023-10-26 12:24:01 +0530

    Feedback changes and QA changes

--- assets/js/dist/custom-head.js
+++ assets/js/dist/custom-head.js
@@ -5933,9 +5933,11 @@ const languageTranslations = {
 
 $(document).ready(function(){
     let language = localStorage.getItem('portalLanguage');
+    console.log(language)
     (language == null || language == 'null') ? setPageLanguage("en") : setPageLanguage(localStorage.getItem('portalLanguage'))
 
     $(".dropdown-wrapper-div").click(function(){
+        alert("click");
         if(($(this).hasClass("open"))){
             $(this).removeClass("open");
             $("i.dropdown").removeClass("rotate");
@@ -5955,6 +5957,7 @@ $(document).ready(function(){
         $("ul.language-dropdown").slideUp("slow");
     });
 })
+
 
 function getLangTranslations() {
     const lang = localStorage.getItem('portalLanguage') || 'en';
