1a2def61d74039c2ca834fa4729738d4287527ed
Muskan Gupta <muskan.gupta@trigyn.com>
2024-02-28 18:22:40 +0530

    Hamburger menu dark mode change & explore button issue fixed

--- assets/js/default/common.js
+++ assets/js/default/common.js
@@ -73,7 +73,7 @@ $(document).ready(function () {
   //quiz telemetry end
   function OnLoad() {
     tenantId = sessionStorage.getItem("tenantSlug") || 'ntp';
-    getOrgInfo(tenantId).done(function () {
+    getOrgInfo(tenantId).done(function () {   
       initTelemetryService();
       let path = window.location.pathname, ftUser = "true";
       if (localStorage && localStorage.getItem("fpDetails_v2")) ftUser = "false";
@@ -94,7 +94,7 @@ $(document).ready(function () {
   OnLoad();
   
   function getChannelId(id){
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => { 
       $.ajax({
         method: "POST",
         url: hostURL + "/api/org/v1/search",
@@ -537,3 +537,58 @@ $(window).resize(function () {
     }
   }
 });
+ 
+
+/*===================== Start focus area slider==============*/
+// $('#focus-area .carousel-item').carousel({
+//   interval: 2000
+// })
+// $(document).ready(function(){  
+//   $('#focus-area .carousel-item').each(function () {
+//     // alert("Hello");
+//     var minPerSlide = 3;
+//     var next = $(this).next();
+//     if (!next.length) {
+//     next = $(this).siblings(':first');
+//     }
+//     next.children(':first-child').clone().appendTo($(this));
+    
+//     for (var i = 0; i < minPerSlide; i++) { next=next.next(); if (!next.length) { next=$(this).siblings(':first'); } next.children(':first-child').clone().appendTo($(this)); } });
+// })
+
+/*=====================End focus area slider==============*/
+
+// $('#recipeCarousel').carousel({ 
+//   interval: 2000
+// })
+$(document).ready(function(){  
+  $('#recipeCarousel .carousel-item').each(function () {   
+    // alert("Hello");
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i = 0; i < minPerSlide; i++) { next=next.next(); if (!next.length) { next=$(this).siblings(':first'); } next.children(':first-child').clone().appendTo($(this)); } });
+})
+
+// $('#recipeCarousel .carousel-item').each(function(){
+//     var minPerSlide = 3;
+//     var next = $(this).next();
+//     if (!next.length) {
+//     next = $(this).siblings(':first');
+//     }
+//     next.children(':first-child').clone().appendTo($(this));
+    
+//     for (var i=0;i<minPerSlide;i++) {
+//         next=next.next();
+//         if (!next.length) {
+//         	next = $(this).siblings(':first');
+//       	}
+//     next.children(':first-child').clone().appendTo($(this));
+//       }
+// });
+/*=====================End diksha dashbord===============*/
+ 
