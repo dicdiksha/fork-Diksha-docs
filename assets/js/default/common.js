a95f67a526e7c0459dd7f333d50867ce3fb4bff7
Samarth Nigam Trigyn <137397906+samarthtrigyn@users.noreply.github.com>
2024-02-29 20:13:27 +0530

    Removed unused code

--- assets/js/default/common.js
+++ assets/js/default/common.js
@@ -540,27 +540,7 @@ $(window).resize(function () {
  
 
 /*===================== Start focus area slider==============*/
-// $('#focus-area .carousel-item').carousel({
-//   interval: 2000
-// })
-// $(document).ready(function(){  
-//   $('#focus-area .carousel-item').each(function () {
-//     // alert("Hello");
-//     var minPerSlide = 3;
-//     var next = $(this).next();
-//     if (!next.length) {
-//     next = $(this).siblings(':first');
-//     }
-//     next.children(':first-child').clone().appendTo($(this));
-    
-//     for (var i = 0; i < minPerSlide; i++) { next=next.next(); if (!next.length) { next=$(this).siblings(':first'); } next.children(':first-child').clone().appendTo($(this)); } });
-// })
 
-/*=====================End focus area slider==============*/
-
-// $('#recipeCarousel').carousel({ 
-//   interval: 2000
-// })
 $(document).ready(function(){  
   $('#recipeCarousel .carousel-item').each(function () {   
     // alert("Hello");
@@ -574,21 +554,5 @@ $(document).ready(function(){
     for (var i = 0; i < minPerSlide; i++) { next=next.next(); if (!next.length) { next=$(this).siblings(':first'); } next.children(':first-child').clone().appendTo($(this)); } });
 })
 
-// $('#recipeCarousel .carousel-item').each(function(){
-//     var minPerSlide = 3;
-//     var next = $(this).next();
-//     if (!next.length) {
-//     next = $(this).siblings(':first');
-//     }
-//     next.children(':first-child').clone().appendTo($(this));
-    
-//     for (var i=0;i<minPerSlide;i++) {
-//         next=next.next();
-//         if (!next.length) {
-//         	next = $(this).siblings(':first');
-//       	}
-//     next.children(':first-child').clone().appendTo($(this));
-//       }
-// });
 /*=====================End diksha dashbord===============*/
  
