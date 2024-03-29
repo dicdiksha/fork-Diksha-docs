ed0450a1cc811394121f85ba14b4ca5ab253ee4b
Muskan Gupta <muskan.gupta@trigyn.com>
2024-03-29 20:27:08 +0530

    144554 Created Landing page for Parents Role in dev

--- assets/js/default/home.js
+++ assets/js/default/home.js
@@ -1120,18 +1120,59 @@ $(document).ready(function(){
       for (var i = 0; i < minPerSlide; i++) { next=next.next(); if (!next.length) { next=$(this).siblings(':first'); } next.children(':first-child').clone().appendTo($(this)); } });
   })
 
-// Nistha Testimonial Carousel 
-let reviewitems = document.querySelectorAll('.nistha-testimonial-carousel .carousel-item')
-reviewitems.forEach((el) => {                        
-const reviewMinPerSlide = 3
-let next = el.nextElementSibling
-for (var review=1; review<reviewMinPerSlide; review++) { 
+// Nistha Testimonial Carousel
+let reviewitems = document.querySelectorAll(
+  ".diksha-teachers-carousel .carousel-item"
+);
+reviewitems.forEach((el) => {
+  const reviewMinPerSlide = 3;
+  let next = el.nextElementSibling;
+  for (var review = 1; review < reviewMinPerSlide; review++) {
     if (!next) {
-// wrap carousel by using first child
-next = reviewitems[0]  
-}
-let cloneChild = next.cloneNode(true)
-el.appendChild(cloneChild.children[0])
-next = next.nextElementSibling 
-}
-});                  
+      // wrap carousel by using first child
+      next = reviewitems[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
+
+// What Parent say about DIKSHA Carousel
+let parentsayitems = document.querySelectorAll(
+  ".parent-say-carousel .carousel-item"
+);
+parentsayitems.forEach((el) => {
+  const parentsayMinPerSlide = 3;
+  let parentsaynext = el.nextElementSibling;
+  for (var parentsay = 1; parentsay < parentsayMinPerSlide; parentsay++) {
+    if (!parentsaynext) {
+      // wrap carousel by using first child
+      parentsaynext = parentsayitems[0];
+    }
+    let cloneChild = parentsaynext.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    parentsaynext = parentsaynext.nextElementSibling;
+  }
+});
+
+
+
+// Diksha for parents Carousel
+let parentstudyitems = document.querySelectorAll(
+  ".parent-study-carousel .carousel-item"
+);
+parentstudyitems.forEach((el) => {
+  const studyMinPerSlide = 3;
+  let studynext = el.nextElementSibling;
+  for (var parentstudy = 1; parentstudy < studyMinPerSlide; parentstudy++) {
+    if (!studynext) {
+      // wrap carousel by using first child
+      studynext = parentstudyitems[0];
+    }
+    let cloneChild = studynext.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    studynext = studynext.nextElementSibling;
+  }
+});
+
